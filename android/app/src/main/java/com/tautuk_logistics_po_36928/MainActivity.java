1d64cc7200cf9b2c0d79fc1de6ce3aed96083796
Crowdbotics <team@crowdbotics.com>
2022-08-26 22:50:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tautuk_logistics_po_36928/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tautuk_logistics_po_36928;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tautuk_logistics_po_36928";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tautuk_logistics_po_36928",
+  url: "https://tautuk_logistics_po_36928.botics.co",
+  api: "https://tautuk_logistics_po_36928.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
